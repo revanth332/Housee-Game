0b1dc09aa9424d6f63c297a24526a6b6dbbacbe9
revathipathilanka347 <revathipathilanka347@gmail.com>
2024-10-23 14:30:23 +0530

    modified socket env

--- src/components/Socket.tsx
+++ src/components/Socket.tsx
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.SOCKET_URL;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,
